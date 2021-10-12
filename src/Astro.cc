64459a942c2cf67eb4a294f4e99f1d9a9d619afd
Enrico Bertolazzi <ebertolazzi@users.noreply.github.com>
2021-10-13 00:56:49 +0200

    corrected format in info for Astro

--- src/Astro.cc
+++ src/Astro.cc
@@ -110,7 +110,7 @@ namespace AstroLib {
       "g     = {}\n"
       "h     = {}\n"
       "k     = {}\n"
-      "Keplerian{}"
+      "Keplerian\n"
       "e     = {}\n"
       "a     = {}\n"
       "i     = {} [degree]\n"
@@ -119,7 +119,9 @@ namespace AstroLib {
       "Other infos"
       "period = {}",
       m_name, (EQ.retrograde?"RETROGRADE":"NORMAL"),
-      t0, M0, Mdot, muS, EQ.p, EQ.f, EQ.g, EQ.h, EQ.k, K.e, K.a,
+      t0, M0, Mdot, muS,
+      EQ.p, EQ.f, EQ.g, EQ.h, EQ.k,
+      K.e, K.a,
       radiants_to_degrees(K.i),
       radiants_to_degrees(K.Omega),
       radiants_to_degrees(K.omega),
