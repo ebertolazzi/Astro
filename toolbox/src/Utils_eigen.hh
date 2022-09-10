837b2b4fbf57a3917484ba5c66e468fc9b6a1b2a
Enrico Bertolazzi <enrico.bertolazzi@unitn.it>
2022-09-10 11:39:04 +0200

    updated submodules

--- toolbox/src/Utils_eigen.hh
+++ toolbox/src/Utils_eigen.hh
@@ -29,10 +29,29 @@
 
 #include "Utils.hh"
 
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-copy-with-dtor"
+#endif
+
+#ifdef _MSC_VER
+#pragma warning( push )
+#pragma warning( disable : 4127 )
+#endif
+
 #define EIGEN_DONT_PARALLELIZE
 #define EIGEN_NO_AUTOMATIC_RESIZING
 
-#include <Eigen/Core>
+#include "Eigen/Core"
+#include "Eigen/Dense"
+
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
+
+#ifdef _MSC_VER
+#pragma warning( pop )
+#endif
 
 #endif
 
