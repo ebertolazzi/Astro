e0b5b347fea65df76f18e8f86b66251541ae6267
Enrico Bertolazzi <enrico.bertolazzi@unitn.it>
2023-07-07 23:48:38 +0200

    added constexpr

--- toolbox/src/Astro/Units.hxx
+++ toolbox/src/Astro/Units.hxx
@@ -30,6 +30,7 @@
 
 namespace AstroLib {
 
+
   /*
   //    ____                              _
   //   / ___|___  _ ____   _____ _ __ ___(_) ___  _ __
@@ -38,20 +39,22 @@ namespace AstroLib {
   //   \____\___/|_| |_|\_/ \___|_|  |___/_|\___/|_| |_|
   */
 
-  extern real_type const one_UA_to_km; // KM
-  extern real_type const one_DAY_to_second; // sec
- 
-  extern real_type const one_km_to_UA;
-  extern real_type const one_m_to_UA;
-  extern real_type const one_second_to_DAY;
- 
-  extern real_type const oneNewton_to_kg_UA_DAY2; // Kg * m/s^2 => Kg * UA / day^2
- 
-  extern real_type const gravity_kg_m_s2;
-  extern real_type const gravity_kg_UA_DAY2;
+  constexpr real_type const one_UA_to_km{1.49597870691E8}; // KM
+  constexpr real_type const one_DAY_to_second{86400.0}; // sec
 
-  extern real_type const muSun_km3s2;   // Km^3/s^2
-  extern real_type const muSun_UA3DAY2; // Km^3/s^2
+  constexpr real_type const one_km_to_UA{1/one_UA_to_km};
+  constexpr real_type const one_m_to_UA{one_km_to_UA/1000};
+  constexpr real_type const one_second_to_DAY{1/one_DAY_to_second};
+
+  constexpr real_type const oneNewton_to_kg_UA_DAY2{one_m_to_UA/(one_second_to_DAY*one_second_to_DAY)}; // Kg * m/s^2 => Kg * UA / day^2
+
+  constexpr real_type const gravity_kg_m_s2{9.80665};
+  constexpr real_type const gravity_kg_UA_DAY2{gravity_kg_m_s2*oneNewton_to_kg_UA_DAY2};
+
+  // costanti gravitazionali
+
+  constexpr real_type const muSun_km3s2{1.32712440018E11}; // Km^3/s^2
+  constexpr real_type const muSun_UA3DAY2{muSun_km3s2*(one_km_to_UA*one_km_to_UA*one_km_to_UA)/(one_second_to_DAY*one_second_to_DAY)}; // Km^3/s^2
 
   // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
